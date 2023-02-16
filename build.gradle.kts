90adf338cdea9ef29e74914fba246576324ddb8f
Elizabeth <foxcapade@gmail.com>
2023-02-16 13:43:03 -0500

    updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,25 @@
 plugins {
   kotlin("jvm") version "1.8.0"
+  `maven-publish`
 }
+
+group = "org.veupathdb.lib"
+version = "1.0.0"
 
 repositories {
   mavenCentral()
 }
 
+java {
+  targetCompatibility = JavaVersion.VERSION_1_8
+  sourceCompatibility = JavaVersion.VERSION_1_8
+
+  withSourcesJar()
+  withJavadocJar()
+}
+
 kotlin {
-  jvmToolchain(18)
+  jvmToolchain(8)
 }
 
 dependencies {
@@ -22,4 +34,60 @@ dependencies {
 
 tasks.test {
   useJUnitPlatform()
+}
+
+tasks.named<Test>("test") {
+  useJUnitPlatform()
+  testLogging {
+    events.addAll(listOf(org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED,
+      org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED,
+      org.gradle.api.tasks.testing.logging.TestLogEvent.STANDARD_OUT,
+      org.gradle.api.tasks.testing.logging.TestLogEvent.STANDARD_ERROR,
+      org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED))
+
+    exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+    showExceptions = true
+    showCauses = true
+    showStackTraces = true
+    showStandardStreams = true
+    enableAssertions = true
+  }
+}
+
+publishing {
+  repositories {
+    maven {
+      name = "GitHub"
+      url  = uri("https://maven.pkg.github.com/veupathdb/maven-packages")
+      credentials {
+        username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+        password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+      }
+    }
+  }
+
+  publications {
+    create<MavenPublication>("gpr") {
+      from(components["java"])
+      pom {
+        name.set("LDAP Utils")
+        description.set("Provides utilities for looking up LDAP records.")
+        url.set("https://github.com/VEuPathDB/lib-ldap-util")
+        developers {
+          developer {
+            id.set("epharper")
+            name.set("Elizabeth Paige Harper")
+            email.set("epharper@upenn.edu")
+            url.set("https://github.com/foxcapades")
+            organization.set("VEuPathDB")
+          }
+        }
+        scm {
+          connection.set("scm:git:git://github.com/VEuPathDB/lib-ldap-util.git")
+          developerConnection.set("scm:git:ssh://github.com/VEuPathDB/lib-ldap-util.git")
+          url.set("https://github.com/VEuPathDB/lib-ldap-util")
+        }
+      }
+    }
+  }
 }
